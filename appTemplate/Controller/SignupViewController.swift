db1ee0cc2f208fe5544eb08dbdb8c2a7ca581514
Victor Hernandez-Urbina <hernandezurbina@gmail.com>
2019-05-20 12:23:15 +0100

    Built basic skeleton

---
+++ appTemplate/Controller/SignupViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignupViewController.swift
+//  appTemplate
+//
+//  Created by Victor Hernandez-Urbina on 20/05/2019.
+//  Copyright © 2019 Victor Hernandez-Urbina. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func signupButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToHome", sender: self)
+    }
+    
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToLogin", sender: self)
+    }
+    
+}
